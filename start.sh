7b886734fc5192507d6bbc839cec0a3c692c85d5
Valikko <help.14x@gmail.com>
2026-06-03 21:13:25 +0000

    fix: download yt-dlp in background to avoid startup timeout

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ if ! which ffmpeg; then
   apt-get update && apt-get install -y ffmpeg
 fi
 if [ ! -f ./yt-dlp ]; then
-  curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp
-  chmod +x ./yt-dlp
+  curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp &
+  chmod +x ./yt-dlp 2>/dev/null || true
 fi
 node server.js
