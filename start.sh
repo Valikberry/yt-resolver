6e28e1e2480985d57856ddb89683d24b1aebc1c9
Valikko <help.14x@gmail.com>
2026-06-03 20:53:21 +0000

    fix: restore yt-dlp download in start.sh for fallback

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 if ! which ffmpeg; then
   apt-get update && apt-get install -y ffmpeg
 fi
+if [ ! -f ./yt-dlp ]; then
+  curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp
+  chmod +x ./yt-dlp
+fi
 node server.js
