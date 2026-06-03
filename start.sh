2e5b7ef16fe709596e07df92c61213b56d2cf162
Valikko <help.14x@gmail.com>
2026-06-03 21:24:22 +0000

    fix: use pip3 to install yt-dlp

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 if ! which ffmpeg; then
   apt-get update && apt-get install -y ffmpeg
 fi
-pip install yt-dlp --break-system-packages -q
+pip3 install yt-dlp --break-system-packages -q 2>/dev/null || python3 -m pip install yt-dlp --break-system-packages -q
 ln -sf $(which yt-dlp) ./yt-dlp 2>/dev/null || true
 node server.js
